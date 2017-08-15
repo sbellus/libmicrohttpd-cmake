555fd4a9400e5c8c8069296c388e583d65cb164f
Štefan Bellus <stevo.bellus@gmail.com>
2017-08-15 16:31:16 +0200

    Build extended for examples, tests and doxygen documentation.

---
+++ libmicrohttpd/cmake/DetermineFork.cmake
@@ -0,0 +1,64 @@
+cmake_minimum_required(VERSION 3.1)
+
+include(CheckIncludeFiles)
+include(CheckCSourceCompiles)
+
+#
+# Macro sets following variables for fork functionality
+# - HAVE_FORK
+# - HAVE_FORK_WAITPID
+#
+macro(determine_fork)
+    check_include_files(sys/types.h HAVE_SYS_TYPES_H)
+    check_include_files(unistd.h HAVE_UNISTD_H)
+    set(old_determine_fork_CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS})
+    if (HAVE_SYS_TYPES_H)
+        list(APPEND CMAKE_REQUIRED_DEFINITIONS -DHAVE_SYS_TYPES_H)
+    endif()
+    if (HAVE_UNISTD_H)
+        list(APPEND CMAKE_REQUIRED_DEFINITIONS -DHAVE_UNISTD_H)
+    endif()
+    
+	check_c_source_compiles("
+        #ifdef HAVE_SYS_TYPES_H
+        #include <sys/types.h>
+        #endif
+        #ifdef HAVE_UNISTD_H
+        #include <unistd.h>
+        #endif
+
+		int main(int argc, char** argv)
+		{
+          pid_t p = fork ();
+          if (0 == p) return 1;
+		 
+		  return 0;
+		}
+		" HAVE_FORK
+	)
+	
+	if (HAVE_FORK)
+    	check_c_source_compiles("
+            #ifdef HAVE_SYS_TYPES_H
+            #include <sys/types.h>
+            #endif
+            #ifdef HAVE_UNISTD_H
+            #include <unistd.h>
+            #endif
+            #include <sys/wait.h>
+    
+    		int main(int argc, char** argv)
+    		{
+              pid_t p = fork ();
+              if (0 == p) return 1;
+    		  
+    		  waitpid (p, (void*)0, 0);
+    		  
+    		  return 0;
+    		}
+    		" HAVE_FORK_WAITPID
+    	)	
+	endif()
+        
+    set(CMAKE_REQUIRED_DEFINITIONS ${old_determine_fork_CMAKE_REQUIRED_DEFINITIONS})
+endmacro()
---
+++ libmicrohttpd/cmake/DetermineStdCHeaders.cmake
@@ -0,0 +1,50 @@
+cmake_minimum_required(VERSION 3.1)
+
+include(CheckIncludeFiles)
+include(CheckSymbolExists)
+include(CheckCSourceRuns)
+
+function(determine_std_c_headers result)
+    check_include_files(stdlib.h HAVE_STDLIB_H)
+    check_include_files(stdarg.h HAVE_STDARG_H)
+    check_include_files(string.h HAVE_STRING_H)
+    check_include_files(float.h HAVE_FLOAT_H)
+    check_symbol_exists(memchr "string.h" HAVE_memchr)
+    check_symbol_exists(free "stdlib.h" HAVE_free)
+
+        
+    if (HAVE_STDLIB_H AND HAVE_STDARG_H AND HAVE_STRING_H AND HAVE_FLOAT_H AND HAVE_memchr AND HAVE_free)
+        if (NOT CMAKE_CROSSCOMPILING)
+            check_c_source_runs("
+                #include <ctype.h>
+                #include <stdlib.h>
+                #if ((' ' & 0x0FF) == 0x020)
+                # define ISLOWER(c) ('a' <= (c) && (c) <= 'z')
+                # define TOUPPER(c) (ISLOWER(c) ? 'A' + ((c) - 'a') : (c))
+                #else
+                # define ISLOWER(c) \
+                		   (('a' <= (c) && (c) <= 'i') \
+                		     || ('j' <= (c) && (c) <= 'r') \
+                		     || ('s' <= (c) && (c) <= 'z'))
+                # define TOUPPER(c) (ISLOWER(c) ? ((c) | 0x40) : (c))
+                #endif
+                
+                #define XOR(e, f) (((e) && !(f)) || (!(e) && (f)))
+                int
+                main ()
+                {
+                  int i;
+                  for (i = 0; i < 256; i++)
+                    if (XOR (islower (i), ISLOWER (i))
+                	|| toupper (i) != TOUPPER (i))
+                      return 2;
+                  return 0;
+                } " STDC_SUPPORT_HIGH_BIT_CHARS)
+            if (STDC_SUPPORT_HIGH_BIT_CHARS)
+                set(${result} 1 PARENT_SCOPE)
+            endif()
+        else()
+            set(${result} 1 PARENT_SCOPE)
+        endif()
+    endif()
+endfunction()
